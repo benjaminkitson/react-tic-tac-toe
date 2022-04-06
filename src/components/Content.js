0135d662243242aeee2a1d0db7ffc6bb1419576e
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-04-06 16:42:27 +0100

    added some semicolons, moved mrRobot and endConditions into separate files

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import Board from './Board';
 import Status from './Status';
 import Modal from './Modal';
-
+import mrRobot from '../utilities/robot';
+import endConditions from '../utilities/endconditions';
 
 function Content() {
 
@@ -12,11 +13,9 @@ function Content() {
     [undefined, undefined, undefined],
     [undefined, undefined, undefined]
   ],)
-
   const [crossesTurn, setCrossesTurn] = useState(true);
   const [gameOver, setGameOver] = useState(false);
   const [winner, setWinner] = useState(undefined);
-
 
   useEffect(() => {
     if (!board.flat().every(square => !square)) {
@@ -34,7 +33,7 @@ function Content() {
 
   const markSquare = (row = undefined, col = undefined) => {
     if (players === '1' && !crossesTurn) {
-      mrRobot(board)
+      mrRobot(board, setBoard)
     } else {
       if (!board[row][col]) {
         const newBoard = [...board];
@@ -46,118 +45,8 @@ function Content() {
     }
   };
 
-
-  const mrRobot = (board) => {
-
-    const newBoard = [...board]
-
-    const victoryAims = {
-      firstRow: [[0, 0], [0, 1], [0, 2]],
-      secondRow: [[1, 0], [1, 1], [1, 2]],
-      thirdRow: [[2, 0], [2, 1], [2, 2]],
-      firstCol: [[0, 0], [1, 0], [2, 0]],
-      secondCol: [[0, 1], [1, 1], [2, 1]],
-      thirdCol: [[0, 2], [1, 2], [2, 2]],
-      firstDiag: [[0, 0], [1, 1], [2, 2]],
-      secondDiag: [[0, 2], [1, 1], [2, 0]]
-    };
-
-    const finishHim = {}
-
-    Object.keys(victoryAims).forEach((option) => {
-      const twoOs = victoryAims[option].filter(square => newBoard[square[0]][square[1]] === 'O').length === 2;
-      const freeSpace = victoryAims[option].some(square => newBoard[square[0]][square[1]] === undefined);
-      if (twoOs && freeSpace) {
-        finishHim[option] = victoryAims[option];
-      }
-    });
-
-    const defenceMode = {}
-
-    Object.keys(victoryAims).forEach((option) => {
-      const twoXs = victoryAims[option].filter(square => newBoard[square[0]][square[1]] === 'X').length === 2;
-      const freeSpace = victoryAims[option].some(square => newBoard[square[0]][square[1]] === undefined);
-      if (twoXs && freeSpace) {
-        defenceMode[option] = victoryAims[option];
-      }
-    });
-
-    let options
-
-    if (Object.keys(finishHim).length) {
-      options = finishHim
-    } else if (Object.keys(defenceMode).length) {
-      options = defenceMode
-    } else {
-      options = {}
-    }
-
-    if (!Object.keys(options).length) {
-      Object.keys(victoryAims).forEach((option) => {
-        const oneO = victoryAims[option].some(square => newBoard[square[0]][square[1]] === 'O');
-        const freeSpace = victoryAims[option].some(square => newBoard[square[0]][square[1]] === undefined);
-        const noXs = victoryAims[option].every(square => newBoard[square[0]][square[1]] !== 'X');
-        if (oneO && freeSpace && noXs) {
-          options[option] = victoryAims[option];
-        }
-      });
-    }
-
-    if (Object.keys(options).length) {
-      const optionsKeys = Object.keys(options);
-      const choice = optionsKeys[Math.floor(Math.random()*optionsKeys.length)];
-      let cpuMarked = false
-      while (cpuMarked === false) {
-        const squareChoice = options[choice][Math.floor(Math.random()*3)];
-        if ((newBoard[squareChoice[0]][squareChoice[1]]) === undefined) {
-          newBoard[squareChoice[0]][squareChoice[1]] = 'O'
-          cpuMarked = true;
-          setTimeout(() => {
-            setBoard(newBoard);
-          }, 500)
-        }
-      }
-    } else {
-      let cpuMarked = false
-      while (cpuMarked === false) {
-        let cpuRow;
-        let cpuCol;
-        if (!board[1][1]) {
-          [cpuRow, cpuCol] = [1, 1];
-        } else {
-          cpuRow = Math.floor(Math.random() * 3);
-          cpuCol = Math.floor(Math.random() * 3);
-        }
-        if (board[cpuRow][cpuCol] === undefined) {
-          const newBoard = [...board];
-          newBoard[cpuRow][cpuCol] = "O";
-          cpuMarked = true;
-          setTimeout(() => {
-            setBoard(newBoard);
-          }, 500)
-        }
-      }
-    }
-
-    // le Robot
-
-  }
-
   const isGameEnd = () => {
-    const endConditions = [
-      // Rows equal
-      (board[0][0] !== undefined) && (board[0][0] === board[0][1]) && (board[0][0] === board[0][2]),
-      (board[1][0] !== undefined) && (board[1][0] === board[1][1]) && (board[1][0] === board[1][2]),
-      (board[2][0] !== undefined) && (board[2][0] === board[2][1]) && (board[2][0] === board[2][2]),
-      // Columns equal
-      (board[0][0] !== undefined) && (board[0][0] === board[1][0]) && (board[0][0] === board[2][0]),
-      (board[0][1] !== undefined) && (board[0][1] === board[1][1]) && (board[0][1] === board[2][1]),
-      (board[0][2] !== undefined) && (board[0][2] === board[1][2]) && (board[0][2] === board[2][2]),
-      // Diagonals
-      (board[0][0] !== undefined) && (board[0][0] === board[1][1]) && (board[0][0] === board[2][2]),
-      (board[0][2] !== undefined) && (board[0][2] === board[1][1]) && (board[0][2] === board[2][0]),
-    ]
-    const didWin = endConditions.some(condition => {
+    const didWin = endConditions(board).some(condition => {
       return condition
     });
     const isTie = board.flat().every(square => square);
@@ -167,7 +56,7 @@ function Content() {
     } else if (isTie) {
       setGameOver(true)
     }
-  }
+  };
 
   const resetGame = () => {
       setGameOver(false);
