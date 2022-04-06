bcd553fe35bfe39e53c8d0701abd608dd7b5a11b
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-04-06 16:01:59 +0100

    updated webpack config with development and production build options

--- webpack.config.js
+++ webpack.config.js
@@ -4,30 +4,36 @@ function buildPath(relative) {
   return path.join(__dirname, relative);
 }
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: buildPath('public'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [{
-      loader: 'babel-loader',
-      test: /\.js$/,
-      exclude: /node_modules/
-    }, {
-      use: [
-        'style-loader',
-        'css-loader',
-        'sass-loader'
-      ],
-      test: /\.s?css$/
-    }]
-  },
-  devtool: 'eval-cheap-module-source-map',
-  devServer: {
-    static: {
-      directory: buildPath('public')
+module.exports = function (env) {
+
+  const isProduction = (env.production === true);
+
+  return {
+    entry: './src/app.js',
+    output: {
+      path: buildPath('public'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [{
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }, {
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ],
+        test: /\.s?css$/
+      }]
+    },
+    devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
+    devServer: {
+      static: {
+        directory: buildPath('public')
+      },
+      historyApiFallback: true
     }
-  }
+  };
 };
