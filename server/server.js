1f9e008f7df60c50f3bae899591d1709a590f884
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-04-07 12:53:39 +0100

    updated package.json with node script

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const publicPath = path.join(__dirname, '..', 'public');
 app.use(express.static(publicPath));
 
 app.get('*', (req, res) => {
-  res.redirect('/')
+  res.redirect('/');
 });
 
 app.listen(3000, () => {
