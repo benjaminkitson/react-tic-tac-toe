b15f269383684149cab014a7255a79a798ecdf36
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-04-07 15:13:37 +0100

    removed Status component, moved essential contents into Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import Status from './Status';
+import React, { useContext } from 'react';
+import AppContext from '../utilities/appcontext';
 
 function Header() {
+
+  const { statusText, resetGame } = useContext(AppContext);
+
   return (
     <div className='header'>
       <h1 className='header__title'>Tic-Tac-Toe!</h1>
-      <Status />
+      <div className='info'>
+        <h1 className="status">{statusText()}</h1>
+        <button className="reset" onClick={resetGame}>Reset</button>
+      </div>
     </div>
   );
 };
 
-export default Header
+export default Header;
