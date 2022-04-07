0dd4919cd44a39915ef4bfbf4394551693fd5365
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-04-07 10:50:40 +0100

    made some tweaks to the server code

--- server/server.js
+++ server/server.js
@@ -6,6 +6,10 @@ const publicPath = path.join(__dirname, '..', 'public');
 
 app.use(express.static(publicPath));
 
+app.get('*', (req, res) => {
+  res.redirect('/')
+});
+
 app.listen(3000, () => {
-  "Tic-Tac-Toe!"
+  console.log("Tic-Tac-Toe!");
 });
